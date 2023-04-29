b0fd05f065ab71adc620463f3c131ecfc56e9bb8
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-04-30 03:35:37 +0700

    update navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,7 +14,10 @@ window.onscroll = function () {
 // humberger
 
 const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
+
 
 humberger.addEventListener('click', function () {
-  humberger.classList.toggle('humberger-active')
+  humberger.classList.toggle('humberger-active');
+  navMenu.classList.toggle('hidden');
 });
